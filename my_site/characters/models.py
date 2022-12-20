8b251b8578129d89a8de74301186981032978414
Eti-Umoh <etiumoh04@gmail.com>
2022-12-20 20:03:23 +0100

    created db models

--- my_site/characters/models.py
+++ my_site/characters/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
 # Create your models here.
+class Characters(models.Model):
+    ch_id = models.CharField(null=True)
+    name = models.CharField(null=True)
+    image = models.ImageField(null=True)
+
+class PowerStats(models.Model):
+    ch_id = models.ForeignKey(Characters,on_delete=models.SET_NULL,null=True)
+    intelligence = models.IntegerField(null=True)
+    strength = models.IntegerField(null=True)
+    speed = models.IntegerField(null=True)
+    durability = models.IntegerField(null=True)
+    power = models.IntegerField(null=True)
+    combat = models.IntegerField(null=True)
+
+class Biography(models.Model):
+    ch_id = models.ForeignKey(Characters,on_delete=models.SET_NULL,null=True)
+    full_name = models.CharField(null=True)
+    alter_ego = models.CharField(null=True)
+    aliases = models.CharField(null=True)
+    place_of_birth = models.CharField(null=True)
+    first_appearance = models.CharField(null=True)
+    publisher = models.CharField(null=True)
+    alignment = models.CharField(null=True)
+
+class Appearance(models.Model):
+    ch_id = models.ForeignKey(Characters,on_delete=models.SET_NULL,null=True)
+    gender = models.CharField(null=True)
+    race = models.CharField(null=True)
+    height = models.CharField(null=True)
+    weight = models.CharField(null=True)
+    eye_color = models.CharField(null=True)
+    hair_color = models.CharField(null=True)
+
+class Work(models.Model):
+    ch_id = models.ForeignKey(Characters,on_delete=models.SET_NULL,null=True)
+    occupation = models.CharField(null=True)
+    base = models.CharField(null=True)
+
+class Connections(models.Model):
+    ch_id = models.ForeignKey(Characters,on_delete=models.SET_NULL,null=True)
+    group_affiliation = models.CharField(null=True)
+    relative = models.CharField(null=True)
+
--- my_site/characters/urls.py
+++ my_site/characters/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path, include
-from . import views
-from rest_framework import routers
+# from django.urls import path, include
+# from . import views
+# from rest_framework import routers
 
-router = routers.DefaultRouter()
-router.register('characters')
+# router = routers.DefaultRouter()
+# router.register('characters')
