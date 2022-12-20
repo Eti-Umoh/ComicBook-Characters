230365483644c07510c31e26498e79502ba0a2ae
Simeone Umoh <simeoneumoh@outlook.com>
2022-12-20 16:47:43 +0100

    updates

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -87,11 +87,11 @@ WSGI_APPLICATION = 'my_site.wsgi.application'
 #     }
 # }
 
-DB_NAME = os.getenv('DB_NAME')
-DB_USER = os.getenv('DB_USER')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_HOST = os.getenv('DB_HOST')
-DB_PORT = os.getenv('DB_PORT')
+# DB_NAME = os.getenv('DB_NAME')
+# DB_USER = os.getenv('DB_USER')
+# DB_PASSWORD = os.getenv('DB_PASSWORD')
+# DB_HOST = os.getenv('DB_HOST')
+# DB_PORT = os.getenv('DB_PORT')
 
 DATABASES = {
     'default': {
